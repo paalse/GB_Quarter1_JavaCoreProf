a1df155b9ee9a9f32acf60bdc6aaee9515dbcc8e
paalse <paalse@list.ru>
2019-10-27 14:02:11 +0400

    Lesson_3

--- src/main/java/Lesson_3/MainTask1.java
+++ src/main/java/Lesson_3/MainTask1.java
@@ -1,4 +1,22 @@
 package Lesson_3;
 
+import java.io.*;
+
+/**
+ * 1. Прочитать файл (около 50 байт) в байтовый массив и вывести этот массив в консоль;
+ */
+
 public class MainTask1 {
+    public static void main(String[] args) {
+        try (InputStreamReader isr = new InputStreamReader(new FileInputStream("TestFile/task1.txt"), "UTF-8")) {
+            int x;
+            while ((x = isr.read()) != -1) {
+                System.out.print((char) x);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/Lesson_3/MainTask3.java
+++ src/main/java/Lesson_3/MainTask3.java
@@ -1,4 +1,44 @@
 package Lesson_3;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Написать консольное приложение, которое умеет постранично читать текстовые файлы (размером > 10 mb).
+ * Вводим страницу (за страницу можно принять 1800 символов), программа выводит ее в консоль.
+ * Контролируем время выполнения: программа не должна загружаться дольше 10 секунд, а чтение – занимать свыше 5 секунд.
+ */
 public class MainTask3 {
+    public static void main(String[] args) {
+
+        try {
+            // Создание файла
+//            FileOutputStream outFile = new FileOutputStream("TestFile/task3.txt");
+//            byte[] arr = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', '\n'};
+//            for (int i = 0; i < 1000000; i++) {
+//                outFile.write(arr);
+//            }
+//            outFile.close();
+
+            // Чтение из файла
+            long t = System.currentTimeMillis();
+            FileInputStream inFile = new FileInputStream("TestFile/task3.txt");
+            int x;
+            byte[] arr1 = new byte[1800];
+
+            while ((x = inFile.read(arr1)) != -1) {
+                  System.out.print(new String(arr1, 0, x, "UTF-8"));
+
+            }
+            inFile.close();
+            System.out.println(System.currentTimeMillis() - t);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
