ed45d8794d86a6af72fdc7a9a5a271e7fdb1f892
paalse <paalse@list.ru>
2019-11-06 16:13:11 +0400

    Задание 1, логирование работы чата.

---
+++ src/main/java/Lesson_6/chat/client/MiniStage.java
@@ -0,0 +1,33 @@
+package Lesson_6.Chat.client;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class MiniStage extends Stage {
+    String nickTo;
+    DataOutputStream out;
+    List<TextArea> parentList;
+
+    public MiniStage(String nickTo, DataOutputStream out, List<TextArea> parentList) {
+        this.nickTo = nickTo;
+        this.out = out;
+        this.parentList = parentList;
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("personal.fxml"));
+            setTitle("personal window");
+            Scene scene = new Scene(root, 300, 400);
+            setScene(scene);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
